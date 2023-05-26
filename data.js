8609d1d1d6066cd2b3843bbdf4580ada00f698e0
aechevarriat <aliciaechevarria27@gmail.com>
2023-05-26 09:53:42 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,604 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-terrraza",
+      "name": "TERRRAZA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8400135643858935,
+          "pitch": 0.21879072496303031,
+          "rotation": 0.7853981633974483,
+          "target": "10-dormitorio2"
+        },
+        {
+          "yaw": -2.7678964074950514,
+          "pitch": 0.3021610627296436,
+          "rotation": 6.283185307179586,
+          "target": "13-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-aseo1",
+      "name": "ASEO1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-aseo21",
+      "name": "ASEO2(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4255749134510483,
+          "pitch": 0.5118341885620321,
+          "rotation": 0,
+          "target": "3-aseo2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-aseo2",
+      "name": "ASEO2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.643645087878844,
+          "pitch": 0.5508757775075228,
+          "rotation": 0,
+          "target": "12-sala-polivalente"
+        },
+        {
+          "yaw": -1.7053817688795405,
+          "pitch": 0.5127235081016064,
+          "rotation": 0,
+          "target": "2-aseo21"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bao1",
+      "name": "BAÑO1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0521471286306765,
+          "pitch": 0.27776300874529625,
+          "rotation": 0,
+          "target": "9-dormitorio1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bao2",
+      "name": "BAÑO2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0205108220381103,
+          "pitch": 0.21263543134089957,
+          "rotation": 0,
+          "target": "10-dormitorio2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-cocina",
+      "name": "COCINA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08482563278953137,
+          "pitch": 0.32094994744810634,
+          "rotation": 0,
+          "target": "13-salon"
+        },
+        {
+          "yaw": 2.822531071119913,
+          "pitch": 0.5679084279448041,
+          "rotation": 0,
+          "target": "11-recibidor"
+        },
+        {
+          "yaw": -2.671635276548093,
+          "pitch": 0.3307416291054288,
+          "rotation": 0,
+          "target": "14-dormitorio3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-coladuria",
+      "name": "COLADURIA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0250502062462505,
+          "pitch": 0.7725769124901074,
+          "rotation": 0,
+          "target": "12-sala-polivalente"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-despacho",
+      "name": "DESPACHO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.062079705438938,
+          "pitch": 0.21629378225937046,
+          "rotation": 0,
+          "target": "11-recibidor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-dormitorio1",
+      "name": "DORMITORIO1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.128257342938971,
+          "pitch": 0.5587464347733455,
+          "rotation": 0,
+          "target": "12-sala-polivalente"
+        },
+        {
+          "yaw": -0.03148144522196006,
+          "pitch": 0.1097243454081962,
+          "rotation": 0,
+          "target": "4-bao1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-dormitorio2",
+      "name": "DORMITORIO2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.036199849668317796,
+          "pitch": 0.09881146315564848,
+          "rotation": 0,
+          "target": "5-bao2"
+        },
+        {
+          "yaw": -0.6662880777441362,
+          "pitch": 0.10556694495214813,
+          "rotation": 4.71238898038469,
+          "target": "0-terrraza"
+        },
+        {
+          "yaw": -2.428517264042796,
+          "pitch": 0.08524294983316594,
+          "rotation": 0.7853981633974483,
+          "target": "13-salon"
+        },
+        {
+          "yaw": 3.0814350579325014,
+          "pitch": 0.5031688205329559,
+          "rotation": 0,
+          "target": "12-sala-polivalente"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-recibidor",
+      "name": "RECIBIDOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0.0119114123382289,
+        "pitch": 0.11273935893214215,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.227103040968796,
+          "pitch": 0.08756514769883239,
+          "rotation": 0.7853981633974483,
+          "target": "8-despacho"
+        },
+        {
+          "yaw": -0.7690758764997163,
+          "pitch": 0.10762495146257578,
+          "rotation": 5.497787143782138,
+          "target": "6-cocina"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-sala-polivalente",
+      "name": "SALA POLIVALENTE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7316665305611565,
+          "pitch": 0.16123213465241193,
+          "rotation": 0.7853981633974483,
+          "target": "0-terrraza"
+        },
+        {
+          "yaw": -0.3293832560983887,
+          "pitch": 0.21795247385073147,
+          "rotation": 0,
+          "target": "7-coladuria"
+        },
+        {
+          "yaw": -1.8173221914200361,
+          "pitch": 0.22690689987089385,
+          "rotation": 0,
+          "target": "10-dormitorio2"
+        },
+        {
+          "yaw": -0.8412840081398549,
+          "pitch": 0.2033352731256972,
+          "rotation": 0,
+          "target": "9-dormitorio1"
+        },
+        {
+          "yaw": 0.3752541792320052,
+          "pitch": 0.21202525356254398,
+          "rotation": 0,
+          "target": "3-aseo2"
+        },
+        {
+          "yaw": 2.6470301076934213,
+          "pitch": 0.23398442262935326,
+          "rotation": 4.71238898038469,
+          "target": "13-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-salon",
+      "name": "SALON",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -0.6867959850452099,
+        "pitch": 0.05394121713226241,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3492642968834847,
+          "pitch": 0.3054869959463673,
+          "rotation": 5.497787143782138,
+          "target": "6-cocina"
+        },
+        {
+          "yaw": 0.25860819347823494,
+          "pitch": 0.1271773410062167,
+          "rotation": 0.7853981633974483,
+          "target": "12-sala-polivalente"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-dormitorio3",
+      "name": "DORMITORIO3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0740847889586735,
+          "pitch": 0.4515396430779006,
+          "rotation": 0.7853981633974483,
+          "target": "6-cocina"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "COLONIAL",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
